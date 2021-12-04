62e5a0baaec49641f0e96bd49cff01ecb9b25d15
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2021-12-04 10:12:14 +0100

    Solution for day 2b

---
+++ day-2b/main.rb
@@ -0,0 +1,19 @@
+position = Struct.new(:depth, :distance, :aim).new(0,0,0)
+
+STDIN.each_line do |line|
+  command, units = line.split
+  case command
+  when 'forward'
+    position.distance += units.to_i
+    position.depth += position.aim * units.to_i
+  when 'down'
+    position.aim += units.to_i
+  when 'up'
+    position.aim -= units.to_i
+  end
+end
+
+puts "horizontal position: #{position.distance}"
+puts "depth: #{position.depth}"
+puts
+puts "product: #{position.distance * position.depth}"
