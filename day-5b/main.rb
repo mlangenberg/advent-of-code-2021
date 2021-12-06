aa8b44c1ce79ac0a7b6ed44f3550e1bac6d8fe95
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2021-12-06 08:20:31 +0100

    Refactor day 5b

--- day-5b/main.rb
+++ day-5b/main.rb
@@ -1,5 +1,3 @@
-require 'matrix'
-
 Point = Struct.new(:x, :y) do
   def to_s; "|#{x}.#{y}"; end
 end
@@ -32,8 +30,7 @@ end
 
 segments = []
 STDIN.each_line do |line|
-  match = line.match(/(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)/)
-  if match
+  if match = line.match(/(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)/)
     segments << Segment.new(*[
       Point.new(match[:x1].to_i, match[:y1].to_i),
       Point.new(match[:x2].to_i, match[:y2].to_i)
@@ -48,26 +45,10 @@ if $VERBOSE
   puts
 end
 
-max_x = segments.map { |s| [s.from.x, s.to.x ] }.flatten.max
-max_y = segments.map { |s| [s.from.y, s.to.y ] }.flatten.max
-
-map = Matrix.zero(max_y + 1, max_x + 1)
-segments.each do |segment|
+points = segments.each_with_object(Hash.new(0)) do |segment, points|
   segment.to_a.each do |point|
-    map[point.y, point.x] += 1
+    points[point] += 1
   end
 end
 
-if $VERBOSE
-  map.row_vectors.each do |row|
-    puts row.map { |i| i == 0 ? '.' : i }.to_a.join
-  end
-end
-
-overlapping_points = 0
-map.each do |e|
-  overlapping_points += 1 if e > 1
-end
-puts
-puts "Overlapping points: #{overlapping_points}"
-
+puts "Overlapping points: #{points.select { |point, count| count > 1 }.size}"
