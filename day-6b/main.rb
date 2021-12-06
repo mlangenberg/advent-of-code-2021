888db747da8abcda42c449bda9768f2e0c4660d6
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2021-12-06 23:10:43 +0100

    Refactor day 6b

--- day-6b/main.rb
+++ day-6b/main.rb
@@ -2,12 +2,9 @@ fishes = STDIN.gets.split(",").map(&:to_i)
 counts_by_age = fishes.each_with_object(Hash.new(0)) { |e,h| h[e] += 1 }
 
 256.times do |day|
-  counts_by_age.dup.each do |age, count|
-    if age == day
-      counts_by_age[day + 7] += counts_by_age.delete(age)
-      counts_by_age[day + 9] += count
-    end
-  end
+  count = counts_by_age.delete(day) || 0
+  counts_by_age[day + 7] += count
+  counts_by_age[day + 9] += count
 end
 
 puts counts_by_age.values.sum
