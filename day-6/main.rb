4dd19c1bcfe12523604b9db9ff1d7039030fc4cf
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2021-12-07 11:20:32 +0100

    Cleanup day 6

---
+++ day-6/main.rb
@@ -0,0 +1,13 @@
+# Run with: `cat input.txt | ruby main.rb 80`
+days = ARGV.shift || 18
+fishes = STDIN.gets.split(',').map(&:to_i)
+counts_by_age = fishes.each_with_object(Hash.new(0)) { |e, h| h[e] += 1 }
+
+days.to_i.times do |day|
+  count = counts_by_age.delete(day) || 0
+  counts_by_age[day + 7] += count
+  counts_by_age[day + 9] += count
+end
+
+puts "After #{days} days:"
+puts counts_by_age.values.sum
