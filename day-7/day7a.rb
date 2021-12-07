682fddcd9ba2023dc03ae9aa6f1bc8ecef06ccd3
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2021-12-07 12:07:29 +0100

    Solution for day 7a

---
+++ day-7/day7a.rb
@@ -0,0 +1,8 @@
+positions = STDIN.gets.split(',').map(&:to_i).sort
+
+middle = positions.length / 2
+median = positions[middle - 1, 2].sum / 2
+
+puts "The median is: #{median}"
+distance = positions.map { |pos| median - pos }.map(&:abs).sum
+puts "Consumed fuel: #{distance}"
