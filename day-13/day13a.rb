a3bde586d8ec513ede311f52e087664820b7bff4
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2022-01-03 11:42:06 +0100

    Solution for day 13a

---
+++ day-13/day13a.rb
@@ -0,0 +1,28 @@
+Dot = Struct.new(:x, :y)
+Fold = Struct.new(:axis, :line)
+
+dots = []
+folds = []
+
+loop do
+  line = ARGF.gets.chomp
+  break if line.empty?
+
+  dots << Dot.new(*line.split(',').map(&:to_i))
+end
+
+ARGF.each_line do |line|
+  match = line.match(/([xy])=(\d+)/)
+  folds << Fold.new(match[1].to_sym, match[2].to_i)
+end
+
+def fold(dots, along)
+  dots.map do |dot|
+    if dot[along.axis] > along.line
+      dot[along.axis] = along.line - (dot[along.axis] - along.line)
+    end
+    dot
+  end
+end
+
+puts fold(dots, folds.shift).uniq.count
