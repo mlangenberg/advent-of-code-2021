353fbc7483e455172b42998668eb8f4071bc2a19
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2022-01-09 17:14:47 +0100

    Move function to Image class

--- day-20/day20.rb
+++ day-20/day20.rb
@@ -1,4 +1,7 @@
 # Run with: `ruby day20.rb input.txt 50` for 50 iterations
+
+require 'forwardable'
+
 PIXELS = {
   '.' => 0,
   '#' => 1
@@ -13,38 +16,47 @@ file.gets # skip line
 input = file.readlines.map(&:chomp).map { |line| line.gsub(/./, PIXELS).chars.map(&:to_i) }
 file.close
 
-image = input.each_with_index.each_with_object(Hash.new(0)) do |(pixels, row), hash|
-  pixels.each_with_index { |pixel, column| hash[[row, column]] = pixel }
-end
+class Image
+  extend Forwardable
+  def_delegators :@pixels, :keys, :values, :[], :[]=, :default, :sort
+  def initialize(default: 0) = @pixels = Hash.new(default)
 
-def enhanced_pixel(image, row, column)
-  indices = (-1..1).to_a.product((-1..1).to_a).map do |offset|
-    [[row, column], offset].transpose.map { |tuple| tuple.reduce(:+) }
+  def enhance
+    output = Image.new(default: ALGORITHM[0].nonzero? && default.zero? ? 1 : 0)
+    range = keys.min.min - 1..keys.max.max + 1
+    range.to_a.repeated_permutation(2).each do |row, column|
+      output[[row, column]] = enhanced_pixel(row:, column:)
+    end
+    output
   end
-  ALGORITHM[indices.map { |index| image[index] }.join.to_i(2)]
-end
 
-def enhance(image)
-  output = Hash.new(ALGORITHM[0].nonzero? && image.default.zero? ? 1 : 0)
-  range = image.keys.min.min - 1..image.keys.max.max + 1
-  range.to_a.repeated_permutation(2).each do |row, column|
-    output[[row, column]] = enhanced_pixel(image, row, column)
+  def enhanced_pixel(row:, column:)
+    indices = (-1..1).to_a.product((-1..1).to_a).map do |offset|
+      [[row, column], offset].transpose.map { |tuple| tuple.reduce(:+) }
+    end
+    ALGORITHM[indices.map { |index| self[index] }.join.to_i(2)]
   end
-  output
-end
 
-def print(image)
-  prev_row = nil
-  image.sort.to_h.each do |(row, _), pixel|
-    putc "\n" and prev_row = row unless prev_row == row
-    putc(pixel.zero? ? '.' : '#')
+  def count_lit_pixels = values.reduce(:+)
+
+  def print
+    prev_row = nil
+    sort.to_h.each do |(row, _), pixel|
+      putc "\n" and prev_row = row unless prev_row == row
+      putc(pixel.zero? ? '.' : '#')
+    end
+    puts
   end
-  puts
 end
 
-print(image) if $VERBOSE
+image = Image.new(default: 0)
+input.each_with_index do |pixels, row|
+  pixels.each_with_index { |value, column| image[[row, column]] = value }
+end
+
+image.print if $VERBOSE
 STEPS.times do
-  image = enhance(image)
-  print(image) if $VERBOSE
+  image = image.enhance
+  image.print if $VERBOSE
 end
-puts "Lit pixels: #{image.values.reduce(:+)}"
+puts "Lit pixels: #{image.count_lit_pixels}"
